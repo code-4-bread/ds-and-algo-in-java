0b356c721cb27d676eacab4c1b7aa10b9aee6667
Code4Bread <iz@kaungmyat.tech>
2020-04-11 23:32:03 +0800

    [C2-P57] high array app scenario changed for binary search

--- src/com/code4bread/HighArray/HighArrayApp.java
+++ src/com/code4bread/HighArray/HighArrayApp.java
@@ -2,17 +2,18 @@ package com.code4bread.HighArray;
 
 public class HighArrayApp {
     public static void main(String[] args) {
-        HighArray harray = new HighArray(3);
+        HighArray harray = new HighArray(10);
 
         harray.insert(1);
         harray.insert(2);
         harray.insert(3);
-        harray.display();
-        harray.delete(2);
-        harray.display();
         harray.insert(4);
-        harray.display();
-        System.out.printf("Found 4 at index %d\n", harray.search(4));
-        harray.display();
+        harray.insert(5);
+        harray.insert(6);
+        harray.insert(7);
+        harray.insert(8);
+        harray.insert(9);
+        harray.insert(10);
+        System.out.println(harray.binarySearch((2)));
     }
 }
